9bb8d3dda2d29e1808ae6ef4bc020597c09076ef
Tian <solidtreepassing@gmail.com>
2024-10-01 11:52:59 +1000

    feat: delete button

--- src/pages/Comment.tsx
+++ src/pages/Comment.tsx
@@ -1,29 +1,57 @@
-import React from 'react';
+import React, { useState } from 'react';
 import avatar from '../assets/images/react-logo.png';
 import '../styles/Comment.scss';
 
+type user = {
+  id: number
+  name: string
+  avatar: string
+}
+
+type commentItem = {
+  commentId: number
+  user: user
+  content: string
+  time: string
+  likes: number
+}
+
+const currentUser: user = {
+  id: 1324324,
+  name: 'React',
+  avatar: avatar
+}
+
 export const Comment: React.FC = () => {
 
-
-  const commentList = [
+  const defaultCommentList: commentItem[] = [
     {
       commentId: 123123,
       user: {
-        userId: 1324324,
+        id: 1324324,
         name: 'React',
         avatar: avatar
       },
       content: 'React is a JavaScript library for building user interfaces.',
       time: '2024-02-02 12:00:00',
       likes: 66,
+    },
+    {
+      commentId: 123124,
+      user: {
+        id: 312312312,
+        name: 'Bugger',
+        avatar: avatar
+      },
+      content: 'Bugger is a JavaScript library for building user interfaces.',
+      time: '2024-02-02 12:00:00',
+      likes: 66,
     }
   ]
 
-  const currentUser = {
-    userId: 1324324,
-    name: 'React',
-    avatar: avatar
-  }
+
+
+  const [commentList, setCommentList] = useState<commentItem[]>(defaultCommentList)
 
 
   return (<div>
@@ -39,7 +67,10 @@ export const Comment: React.FC = () => {
           <img src={avatar} alt="avatar img" />
         </div>
         <div className="replay-box-wrap">
-          <textarea name="" id="" className="replay-box-textarea"></textarea>
+          <textarea
+            name=""
+            id=""
+            className="replay-box-textarea"></textarea>
           <button className="replay-box-btn">
             <span className="replay-box-btn-text">Send</span>
           </button>
@@ -47,29 +78,50 @@ export const Comment: React.FC = () => {
       </div>
 
       <div className="comment-list">
-        <div className="comment-item">
-          <div className="comment-item-avatar">
-            <img src={avatar} alt="avatar img" />
-          </div>
-          <div className="comment-item-content">
-            <div className="user-info">
-              <span className="user-info-name">React</span>
-            </div>
-            <div className="reply-text">
-              <p>
-                React is a JavaScript library for building user interfaces.
-              </p>
-            </div>
-            <div className="reply-info">
-              <span className="reply-info-time">2024-02-02 12:00:00</span>
-              <span className="reply-info-text">Likes: 66</span>
-              <span className="reply-info-delete">Delete</span>
-            </div>
-          </div>
-        </div>
+
+        {commentList.map((comment) => {
+          return (
+            <CommentItem
+              key={comment.commentId}
+              item={comment}
+              onDelete={(commentId: number) => {
+                setCommentList(commentList.filter((item) => item.commentId !== commentId))
+              }}
+            />
+          )
+        })}
       </div>
-
     </div>
-
   </div>)
 }
+
+const CommentItem: React.FC<{
+  item: commentItem
+  onDelete: (commentId: number) => void
+}> = ({ item, onDelete }) => {
+  return (
+    <div className="comment-item">
+      <div className="comment-item-avatar">
+        <img src={item.user.avatar} alt="avatar img" />
+      </div>
+      <div className="comment-item-content">
+        <div className="user-info">
+          <span className="user-info-name">{item.user.name}</span>
+        </div>
+        <div className="reply-text">
+          <p>
+            {item.content}
+          </p>
+        </div>
+        <div className="reply-info">
+          <span className="reply-info-time">{item.time}</span>
+          <span className="reply-info-text">Likes: {item.likes}</span>
+          {item.user.id === currentUser.id && (
+            <span className="reply-info-delete" onClick={() => onDelete(item.commentId)}>Delete</span>
+          )}
+        </div>
+      </div>
+    </div>
+  )
+}
+
