6d5cc23928482a034cd236bd72aaef9f44484067
Tian <solidtreepassing@gmail.com>
2024-10-22 22:34:09 +1100

    router: lazy loading

--- src/router.tsx
+++ src/router.tsx
@@ -1,41 +1,55 @@
 import TodoList from './pages/testPages/TodoList';
 import { Navigation } from './pages/testPages/Navigation';
-import DashBoard from './pages/DashBoard';
 import { createBrowserRouter } from 'react-router-dom';
-import Home from './pages/Home';
-import Quiz from './pages/Quiz';
-import Profile from './pages/Profile';
-import NotFound from './pages/NotFound';
-import Login from './pages/Login';
-import AuthRoute from './pages/AuthRoute';
+import { lazy, Suspense } from 'react';
+
+const DashBoard = lazy(() => import('./pages/DashBoard'));
+const NotFound = lazy(() => import('./pages/NotFound'));
+const Login = lazy(() => import('./pages/Login'));
+const AuthRoute = lazy(() => import('./pages/AuthRoute'));
+const Home = lazy(() => import('./pages/Home'));
+const Quiz = lazy(() => import('./pages/Quiz'));
+const TrashBin = lazy(() => import('./pages/TrashBin'));
+
+function suspenseWrapper(component: React.ReactNode) {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      {component}
+    </Suspense>
+  )
+}
 
 export const router = createBrowserRouter([
   {
     path: '/',
-    errorElement: <NotFound />,
+    errorElement: suspenseWrapper(<NotFound />),
     element: (
       <AuthRoute>
-        <DashBoard />
+        {suspenseWrapper(<DashBoard />)}
       </AuthRoute>
     ),
     children: [
       {
         path: 'home',
-        element: <Home />,
+        element: suspenseWrapper(<Home />),
       },
       {
         path: 'quiz',
-        element: <Quiz />,
+        element: suspenseWrapper(<Quiz />),
       },
       {
-        path: 'profile',
-        element: <Profile />,
+        path: 'trash-bin',
+        element: suspenseWrapper(<TrashBin />),
+      },
+      {
+        path: 'about',
+        element: suspenseWrapper(<div>About</div>),
       },
     ]
   },
   {
     path: 'login',
-    element: <Login />,
+    element: suspenseWrapper(<Login />),
   },
   {
     path: '/nav',
